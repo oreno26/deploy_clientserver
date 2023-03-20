4ec6556d9b3ef0505f5e434f32bc6bcd61add1fd
Oreno26 <orendanger@hotmail.com>
2023-03-20 10:57:09 +0200

    fst

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require('cors');
+const path = require('path')
+
+const app = express();
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Orens  server" });
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server listening on ${process.env.PORT || 3001}`);
+});
+
+app.use(express.static(path.join(__dirname,'client/build')))
+
+app.get('*', (req,res)=>{
+  res.sendFile(path.resolve(__dirname,'./client/build','index.html'))
+})
